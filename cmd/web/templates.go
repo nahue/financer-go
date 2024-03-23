45f91963caf0f2b7976f35694effea997c8684bd
Nahuel Chaves <nahuel.chaves@gmail.com>
2024-03-23 02:52:07 -0300

    update

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,7 +1,46 @@
 package main
 
-import "github.com/nahue/financer-go/internal/models"
+import (
+	"path/filepath"
+	"text/template"
+
+	"github.com/nahue/financer-go/internal/models"
+)
 
 type templateData struct {
-	Snippet models.Snippet
+	CurrentYear int
+	Snippet     models.Snippet
+	Snippets    []models.Snippet
+}
+
+func newTemplateCache() (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./ui/html/pages/*.tmpl")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+
+		ts, err := template.ParseFiles("./ui/html/base.tmpl")
+		if err != nil {
+			return nil, err
+		}
+
+		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl")
+		if err != nil {
+			return nil, err
+		}
+
+		ts, err = ts.ParseFiles(page)
+		if err != nil {
+			return nil, err
+		}
+
+		cache[name] = ts
+	}
+
+	return cache, nil
 }
